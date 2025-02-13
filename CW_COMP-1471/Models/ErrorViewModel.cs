10b576df65ad99ad56c5ad21242ba8a5036bd65a
mimi-kapadia12 <mansikapadia1999@gmail.com>
2025-02-13 15:22:48 +0000

    Add project files.

---
+++ CW_COMP-1471/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CW_COMP_1471.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
